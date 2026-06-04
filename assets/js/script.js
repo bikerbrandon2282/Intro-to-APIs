a37f0bac306b70bb2b2c536625e22c229777c965
bikerbrandon2282 <bikerbrandon2282@gmail.com>
2026-06-04 20:13:55 +0100

    adding javascript to check status of API key and display in Modal

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+
+const API_KEY ="tM05mA69Jpab1_4Th77nGL2KPQA";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+    const response = await fetch(queryString);
+    const data = await response.json();
+    if (response.ok) {
+        displayStatus(data);
+    }
+    else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+    document.getElementById("resultsModalTitle").innerText = "API Key Status";
+    document.getElementById("results-content").innerHTML = `<div>Your API key is valid until</div><div class="key-status">${data.expiry}</div>`;
+    resultsModal.show();
+}
